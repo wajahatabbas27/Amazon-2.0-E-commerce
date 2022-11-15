d7a77b79d6410f7b5c1b985a7769d518d1b1e63b
wajahatabbas27 <wajahatabbas27@gmail.com>
2022-11-15 06:43:11 +0500

    added the next auth for facebook and google

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/firestore";
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBw7hbZ92wNVw8rX-ytqVazS26jm0wqkL0",
+  authDomain: "fir-75e60.firebaseapp.com",
+  projectId: "fir-75e60",
+  storageBucket: "fir-75e60.appspot.com",
+  messagingSenderId: "700116461306",
+  appId: "1:700116461306:web:c53b9ac46463615cde4b43",
+  measurementId: "G-NY9WGN7KNC",
+};
